a40e362ea1206f8d0a003e50a8e336afeb176abe
Mobin2019331044 <kamrujjamanmobin123@gmail.com>
2023-08-14 23:57:46 +0600

    login page update

--- server.js
+++ server.js
@@ -9,11 +9,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view-engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static('assets'));
 
 app.post("/by", (req, res) => {});
 
 app.get("/", (req, res) => {
     res.render("login.ejs");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs");
+});
+
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard.ejs");
 });
 
 app.listen(port, () => {
